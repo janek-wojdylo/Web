777e2d139ccfb3272d166c13e8ba6133d7312e3e
Jan Wojdyło <janek.wojdylo11@gmail.com>
2020-05-11 13:47:32 +0200

    new homepage

---
+++ main.js
@@ -0,0 +1,13 @@
+const name = 'Janek';
+const age = 34;
+
+
+console.log(name);
+console.log(age);
+console.log(`Hej, nazywam się ${name} i mam ${age}lata`);
+    
+
+const heading = document.querySelector(`Hej, nazywam się ${name} i mam ${age}lata`);
+
+heading.innerHTML = `Hej, nazywam się ${name} i mam ${age}lata;
+
