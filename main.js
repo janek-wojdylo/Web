0f2a28169c2006fb1e5e5af98f428762bcf3581c
Jan Wojdyło <janek.wojdylo11@gmail.com>
2020-05-12 00:51:39 +0200

    correct

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const name = 'Janek';
 const age = 34;
 
-
-console.log(name);
-console.log(age);
 console.log(`Hej, nazywam się ${name} i mam ${age}lata`);
     
 
-const heading = document.querySelector(`Hej, nazywam się ${name} i mam ${age}lata`);
 
-heading.innerHTML = `Hej, nazywam się ${name} i mam ${age}lata;
 
+
+
+function calculate(myNumber) {
+    console.log(`Dostałam ${myNumber}`);
+    myNumber = myNumber * 7;
+    return myNumber;
+}
+const myResult = calculate(age);
+
+console.log(myResult);
+console.log(age);
